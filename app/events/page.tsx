8a53f84c08f0aa691c34ce796f881fbb9c42adc5
parasquid <git@parasquid.com>
2024-07-02 14:56:31 +0800

    add events page

---
+++ app/events/page.tsx
@@ -0,0 +1,350 @@
+export default function EventsPage() {
+  return (
+    <main className="main-wrapper">
+      <section className="work-section">
+        <div className="padding-global">
+          <div className="container-medium">
+            <div className="padding-section-large">
+              <div className="margin-bottom margin-xxlarge">
+                <div className="text-align-center">
+                  <div className="max-width-large align-center">
+                    <div className="margin-bottom margin-small">
+                      <h1>Hivekind Events</h1>
+                    </div>
+                    <p className="text-size-medium">
+                      We host meetups at our office where we discuss software
+                      development, product management, and a variety of other
+                      topics that interest us. Come for the engaging discussions
+                      (or food) and stay for the networking opportunities!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="event_component">
+                <div className="event_list-wrapper">
+                  <div className="event_list">
+                    <div
+                      id="w-node-bea32745-2804-d33e-6027-f6de12c486fd-84e57cca"
+                      className="event_item"
+                    >
+                      <div className="event_image-wrapper">
+                        <div className="ruby-tuesdays">
+                          <img
+                            src="https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6658146b3f1e1abfde477499_ruby-kl-logo.jpg"
+                            loading="lazy"
+                            alt="Ruby KL logo"
+                            className="ruby_tuesdays-image"
+                          />
+                        </div>
+                      </div>
+                      <div className="event_item-content">
+                        <div className="event_item-content-left">
+                          <div className="event_item-content-top">
+                            <div className="event_title">
+                              <div className="heading-style-h5">
+                                Ruby Tuesdays #107
+                              </div>
+                              <div className="event4_tag-item"></div>
+                            </div>
+                            <div className="event_date-wrapper">
+                              <div className="text-size-small">7 May 2024</div>
+                            </div>
+                          </div>
+                          <div className="event_item-content-details">
+                            <div className="event-agenda">
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>7:30 - 8:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Registration &amp; Welcome and intro to Ruby
+                                    Tuesdays
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:00 - 8:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Anonoz: Introducing OyenCov: and a few of my
+                                    random hacks in developing Rails apps
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:30 - 9:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Guoy: Instance Variable in an Object Shape
+                                    World
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>9:00 - 9:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>Parking Lot / Today I Learned !!!</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <a
+                          href="https://www.youtube.com/watch?v=XuucPsney-g"
+                          target="_blank"
+                          className="button is-event w-button"
+                        >
+                          Watch Recording
+                        </a>
+                      </div>
+                    </div>
+                    <div
+                      id="w-node-_6b2cde9d-0aa4-f225-5c2f-e741fc272cea-84e57cca"
+                      className="event_item"
+                    >
+                      <div className="event_image-wrapper">
+                        <div className="ruby-tuesdays">
+                          <img
+                            src="https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6658146b3f1e1abfde477499_ruby-kl-logo.jpg"
+                            loading="lazy"
+                            alt="Ruby KL logo"
+                            className="ruby_tuesdays-image"
+                          />
+                        </div>
+                      </div>
+                      <div className="event_item-content">
+                        <div className="event_item-content-left">
+                          <div className="event_item-content-top">
+                            <div className="event_title">
+                              <div className="heading-style-h5">
+                                Ruby Tuesdays #105
+                              </div>
+                              <div className="event4_tag-item"></div>
+                            </div>
+                            <div className="event_date-wrapper">
+                              <div className="text-size-small">
+                                5 March 2024
+                              </div>
+                            </div>
+                          </div>
+                          <div className="event_item-content-details">
+                            <div className="event-agenda">
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>7:30 - 8:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Registration &amp; Welcome and intro to Ruby
+                                    Tuesdays
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:00 - 8:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Hassanin Ahmed: Your app as a framework
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:30 - 9:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>Yow Chun: Hotwiring your rails app</div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>9:00 - 9:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>Parking Lot / Today I Learned !!!</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <a
+                          href="https://www.youtube.com/watch?v=hORNzvK9y2o"
+                          target="_blank"
+                          className="button is-event w-button"
+                        >
+                          Watch Recording
+                        </a>
+                      </div>
+                    </div>
+                    <div
+                      id="w-node-fdc5dae7-9853-1478-4c87-16a1eabfae9c-84e57cca"
+                      className="event_item"
+                    >
+                      <div className="event_image-wrapper">
+                        <div className="ruby-tuesdays">
+                          <img
+                            src="https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6658146b3f1e1abfde477499_ruby-kl-logo.jpg"
+                            loading="lazy"
+                            alt="Ruby KL logo"
+                            className="ruby_tuesdays-image"
+                          />
+                        </div>
+                      </div>
+                      <div className="event_item-content">
+                        <div className="event_item-content-left">
+                          <div className="event_item-content-top">
+                            <div className="event_title">
+                              <div className="heading-style-h5">
+                                Ruby Tuesdays #103
+                              </div>
+                              <div className="event4_tag-item"></div>
+                            </div>
+                            <div className="event_date-wrapper">
+                              <div className="text-size-small">
+                                5 December 2023
+                              </div>
+                            </div>
+                          </div>
+                          <div className="event_item-content-details">
+                            <div className="event-agenda">
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>7:30 - 8:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Registration &amp; Welcome and intro to Ruby
+                                    Tuesdays
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:00 - 9:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    2023 retrospective: Amree Zaid, Wan Zul,
+                                    Helio Cola, and Yow Chun
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>9:00 - 9:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>Parking Lot / Today I Learned !!!</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <a
+                          href="https://www.youtube.com/watch?v=klJPaXyAShs"
+                          target="_blank"
+                          className="button is-event w-button"
+                        >
+                          Watch Recording
+                        </a>
+                      </div>
+                    </div>
+                    <div
+                      id="w-node-_5cc3c841-be1e-e0da-73bc-a51bbe06274d-84e57cca"
+                      className="event_item"
+                    >
+                      <div className="event_image-wrapper">
+                        <div className="ruby-tuesdays">
+                          <img
+                            src="https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6658146b3f1e1abfde477499_ruby-kl-logo.jpg"
+                            loading="lazy"
+                            alt="Ruby KL logo"
+                            className="ruby_tuesdays-image"
+                          />
+                        </div>
+                      </div>
+                      <div className="event_item-content">
+                        <div className="event_item-content-left">
+                          <div className="event_item-content-top">
+                            <div className="event_title">
+                              <div className="heading-style-h5">
+                                Ruby Tuesdays #101
+                              </div>
+                              <div className="event4_tag-item"></div>
+                            </div>
+                            <div className="event_date-wrapper">
+                              <div className="text-size-small">
+                                3 October 2023
+                              </div>
+                            </div>
+                          </div>
+                          <div className="event_item-content-details">
+                            <div className="event-agenda">
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>7:30 - 8:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Registration &amp; Welcome and intro to Ruby
+                                    Tuesdays
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:00 - 8:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>
+                                    Guoy: Looks like a talk, sounds like a talk
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>8:30 - 9:00</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>Helio Cola: app subdomain routing</div>
+                                </div>
+                              </div>
+                              <div className="agenda-item">
+                                <div className="agenda-time">
+                                  <div>9:00 - 9:30</div>
+                                </div>
+                                <div className="agenda-details">
+                                  <div>Parking Lot / Today I Learned !!!</div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <a
+                          href="https://www.youtube.com/watch?v=qWs-WE8Sv3Q"
+                          target="_blank"
+                          className="button is-event w-button"
+                        >
+                          Watch Recording
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
