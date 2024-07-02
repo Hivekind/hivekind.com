5ab4bb42eb3c262cecc817395e1b3109283d6405
parasquid <git@parasquid.com>
2024-07-02 15:01:51 +0800

    add referral program page

---
+++ app/referral-program/page.tsx
@@ -0,0 +1,370 @@
+export default function ReferralProgramPage() {
+  return (
+    <main className="main-wrapper">
+      <header className="header-section">
+        <div className="padding-global">
+          <div className="container-large">
+            <div className="padding-section-large referral">
+              <div className="w-layout-grid header-component">
+                <div className="header-content">
+                  <div className="margin-bottom margin-small">
+                    <h1>
+                      Software development <br />
+                      referral program
+                    </h1>
+                  </div>
+                  <p className="text-size-medium">
+                    Know someone who needs our
+                    <a href="/services">services</a>? Refer them to us and earn
+                    2%-5% of their monthly billings as a reward.
+                  </p>
+                  <div className="margin-top margin-medium">
+                    <div className="button-group">
+                      <a
+                        href="mailto:hello@hivekind.com"
+                        className="button w-button"
+                      >
+                        Refer a client
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="header_image-wrapper">
+                  <img
+                    src="https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6657ffeeb0ed2035fbfc99ff_software-development-referral-program.jpg"
+                    loading="eager"
+                    sizes="(max-width: 479px) 100vw, (max-width: 991px) 90vw, 41vw"
+                    srcSet="
+                    https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6657ffeeb0ed2035fbfc99ff_software-development-referral-program-p-500.jpg   500w,
+                    https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6657ffeeb0ed2035fbfc99ff_software-development-referral-program-p-800.jpg   800w,
+                    https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6657ffeeb0ed2035fbfc99ff_software-development-referral-program-p-1080.jpg 1080w,
+                    https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6657ffeeb0ed2035fbfc99ff_software-development-referral-program-p-1600.jpg 1600w,
+                    https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/6657ffeeb0ed2035fbfc99ff_software-development-referral-program.jpg        1893w
+                  "
+                    alt="A woman holding an iPad"
+                    className="header-image referral"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section className="steps-section background-color-white">
+        <div className="padding-global">
+          <div className="container-large">
+            <div className="padding-section-large">
+              <div className="steps_component">
+                <div className="margin-bottom margin-xxlarge">
+                  <div className="text-align-center">
+                    <div className="max-width-large">
+                      <h2>Here&#x27;s how the program works</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-layout-grid steps_list">
+                  <div className="steps_item">
+                    <div className="margin-bottom margin-small">
+                      <div className="step-number">
+                        <div className="text-size-large text-weight-bold">
+                          1
+                        </div>
+                      </div>
+                    </div>
+                    <div className="margin-bottom margin-small">
+                      <h3 className="heading-style-h5">
+                        Connect us with a client
+                      </h3>
+                    </div>
+                    <p>
+                      Refer us to someone who needs our
+                      <a href="/services">services</a>. Make introductions to
+                      <a href="mailto:hello@hivekind.com">hello@hivekind.com</a>
+                      and we&#x27;ll take it from there.
+                    </p>
+                  </div>
+                  <div className="steps_item">
+                    <div className="margin-bottom margin-small">
+                      <div className="step-number">
+                        <div className="text-size-large text-weight-bold">
+                          2
+                        </div>
+                      </div>
+                    </div>
+                    <div className="margin-bottom margin-small">
+                      <h3 className="heading-style-h5">
+                        Get your first payment
+                      </h3>
+                    </div>
+                    <p>
+                      Once we sign a contract with the client you introduced and
+                      they make their first payment, you will receive 5% of
+                      their first month&#x27;s billing.
+                    </p>
+                  </div>
+                  <div className="steps_item">
+                    <div className="margin-bottom margin-small">
+                      <div className="step-number">
+                        <div className="text-size-large text-weight-bold">
+                          3
+                        </div>
+                      </div>
+                    </div>
+                    <div className="margin-bottom margin-small">
+                      <h3 className="heading-style-h5">
+                        Keep earning every month
+                      </h3>
+                    </div>
+                    <p>
+                      You will receive 5% of the monthly billings from the
+                      client you referred to us for the first 12 months and 2%
+                      thereafter for the entire duration of our contract with
+                      them.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="faq-section">
+        <div className="padding-global">
+          <div className="container-small">
+            <div className="padding-section-large">
+              <div className="margin-bottom margin-xxlarge">
+                <div className="text-align-center">
+                  <div className="max-width-large">
+                    <h2>Frequently asked questions</h2>
+                  </div>
+                </div>
+              </div>
+              <div className="faq_component">
+                <div className="w-layout-grid faq_list">
+                  <div className="faq_accordion">
+                    <div
+                      data-w-id="9d3fd308-b648-bf16-664d-ca74721db599"
+                      className="faq_question"
+                    >
+                      <div className="text-size-medium text-weight-bold">
+                        How much can I earn?
+                      </div>
+                      <div className="faq_icon-wrappper">
+                        <div className="icon-embed-small w-embed">
+                          <svg
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 32 32"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M25.3333 15.667V16.3336C25.3333 16.7018 25.0349 17.0003 24.6667 17.0003H17V24.667C17 25.0351 16.7015 25.3336 16.3333 25.3336H15.6667C15.2985 25.3336 15 25.0351 15 24.667V17.0003H7.3333C6.96511 17.0003 6.66663 16.7018 6.66663 16.3336V15.667C6.66663 15.2988 6.96511 15.0003 7.3333 15.0003H15V7.33365C15 6.96546 15.2985 6.66699 15.6667 6.66699H16.3333C16.7015 6.66699 17 6.96546 17 7.33365V15.0003H24.6667C25.0349 15.0003 25.3333 15.2988 25.3333 15.667Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{ width: "100%", height: 0 }}
+                      className="faq_answer"
+                    >
+                      <div className="margin-bottom margin-small">
+                        <p>
+                          If the monthly billing for the client you introduced
+                          to us is USD 30,000, then you will earn USD 1,500 per
+                          month for the first 12 months and USD 600 per month
+                          thereafter for the entire duration of our contract
+                          with them.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="faq_accordion">
+                    <div
+                      data-w-id="9d3fd308-b648-bf16-664d-ca74721db5a3"
+                      className="faq_question"
+                    >
+                      <div className="text-size-medium text-weight-bold">
+                        How do I get paid?
+                      </div>
+                      <div className="faq_icon-wrappper">
+                        <div className="icon-embed-small w-embed">
+                          <svg
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 32 32"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M25.3333 15.667V16.3336C25.3333 16.7018 25.0349 17.0003 24.6667 17.0003H17V24.667C17 25.0351 16.7015 25.3336 16.3333 25.3336H15.6667C15.2985 25.3336 15 25.0351 15 24.667V17.0003H7.3333C6.96511 17.0003 6.66663 16.7018 6.66663 16.3336V15.667C6.66663 15.2988 6.96511 15.0003 7.3333 15.0003H15V7.33365C15 6.96546 15.2985 6.66699 15.6667 6.66699H16.3333C16.7015 6.66699 17 6.96546 17 7.33365V15.0003H24.6667C25.0349 15.0003 25.3333 15.2988 25.3333 15.667Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{ width: "100%", height: 0 }}
+                      className="faq_answer"
+                    >
+                      <div className="margin-bottom margin-small">
+                        <p>
+                          Your earnings will be transferred to your personal
+                          bank account. If you&#x27;re an existing client of
+                          Hivekind, we can convert it to monthly engineering
+                          credits.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="faq_accordion">
+                    <div
+                      data-w-id="9d3fd308-b648-bf16-664d-ca74721db5ad"
+                      className="faq_question"
+                    >
+                      <div className="text-size-medium text-weight-bold">
+                        Who can join this program?
+                      </div>
+                      <div className="faq_icon-wrappper">
+                        <div className="icon-embed-small w-embed">
+                          <svg
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 32 32"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M25.3333 15.667V16.3336C25.3333 16.7018 25.0349 17.0003 24.6667 17.0003H17V24.667C17 25.0351 16.7015 25.3336 16.3333 25.3336H15.6667C15.2985 25.3336 15 25.0351 15 24.667V17.0003H7.3333C6.96511 17.0003 6.66663 16.7018 6.66663 16.3336V15.667C6.66663 15.2988 6.96511 15.0003 7.3333 15.0003H15V7.33365C15 6.96546 15.2985 6.66699 15.6667 6.66699H16.3333C16.7015 6.66699 17 6.96546 17 7.33365V15.0003H24.6667C25.0349 15.0003 25.3333 15.2988 25.3333 15.667Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{ width: "100%", height: 0 }}
+                      className="faq_answer"
+                    >
+                      <div className="margin-bottom margin-small">
+                        <p>
+                          Anyone above the age of 18. If you know somebody who
+                          needs a reliable dev team, then you’re already a good
+                          fit.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="faq_accordion">
+                    <div
+                      data-w-id="9d3fd308-b648-bf16-664d-ca74721db5b7"
+                      className="faq_question"
+                    >
+                      <div className="text-size-medium text-weight-bold">
+                        Who is a good referral?
+                      </div>
+                      <div className="faq_icon-wrappper">
+                        <div className="icon-embed-small w-embed">
+                          <svg
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 32 32"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M25.3333 15.667V16.3336C25.3333 16.7018 25.0349 17.0003 24.6667 17.0003H17V24.667C17 25.0351 16.7015 25.3336 16.3333 25.3336H15.6667C15.2985 25.3336 15 25.0351 15 24.667V17.0003H7.3333C6.96511 17.0003 6.66663 16.7018 6.66663 16.3336V15.667C6.66663 15.2988 6.96511 15.0003 7.3333 15.0003H15V7.33365C15 6.96546 15.2985 6.66699 15.6667 6.66699H16.3333C16.7015 6.66699 17 6.96546 17 7.33365V15.0003H24.6667C25.0349 15.0003 25.3333 15.2988 25.3333 15.667Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{ width: "100%", height: 0 }}
+                      className="faq_answer"
+                    >
+                      <div className="margin-bottom margin-small">
+                        <p>
+                          A promising startup or business looking to hire a
+                          development team to build their product.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="faq_accordion">
+                    <div
+                      data-w-id="9d3fd308-b648-bf16-664d-ca74721db5c1"
+                      className="faq_question"
+                    >
+                      <div className="text-size-medium text-weight-bold">
+                        Why should I recommend Hivekind?
+                      </div>
+                      <div className="faq_icon-wrappper">
+                        <div className="icon-embed-small w-embed">
+                          <svg
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 32 32"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M25.3333 15.667V16.3336C25.3333 16.7018 25.0349 17.0003 24.6667 17.0003H17V24.667C17 25.0351 16.7015 25.3336 16.3333 25.3336H15.6667C15.2985 25.3336 15 25.0351 15 24.667V17.0003H7.3333C6.96511 17.0003 6.66663 16.7018 6.66663 16.3336V15.667C6.66663 15.2988 6.96511 15.0003 7.3333 15.0003H15V7.33365C15 6.96546 15.2985 6.66699 15.6667 6.66699H16.3333C16.7015 6.66699 17 6.96546 17 7.33365V15.0003H24.6667C25.0349 15.0003 25.3333 15.2988 25.3333 15.667Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{ width: "100%", height: 0 }}
+                      className="faq_answer"
+                    >
+                      <div className="margin-bottom margin-small">
+                        <p>
+                          We don&#x27;t want to tell you why you should
+                          recommend us. Instead,
+                          <a
+                            href="https://clutch.co/profile/hivekind#reviews"
+                            target="_blank"
+                          >
+                            click here
+                          </a>
+                          to see why our clients recommend us.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="margin-top margin-xxlarge">
+                <div className="text-align-center">
+                  <div className="max-width-medium align-center">
+                    <div className="margin-bottom margin-xsmall">
+                      <h3>Still have questions?</h3>
+                    </div>
+                    <p className="text-size-medium">
+                      Feel free to reach out to our team for more information.
+                    </p>
+                    <div className="margin-top margin-medium">
+                      <a
+                        href="mailto:attraction@hivekind.com?subject=Referral%20enquiry"
+                        className="button w-button"
+                      >
+                        Contact us
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
