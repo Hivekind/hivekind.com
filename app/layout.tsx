3e39bb6cdeb5de636fa82b481a930654bb6c93a1
Tee Shuwn Yuan <s.yuan31tee@gmail.com>
2024-07-05 19:33:16 +0800

    Fix Menu not working in Mobile layout. The reason is due to jquery not downloaded properly. Also we will host webflow js ourselves

--- app/layout.tsx
+++ app/layout.tsx
@@ -344,12 +344,13 @@ export default function RootLayout({
       <Script
         src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6347cb105849aecae0fd4ed8"
         type="text/javascript"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
       ></Script>
+
       <Script
-        src="https://cdn.prod.website-files.com/6347cb105849aecae0fd4ed8/js/webflow.7076b6f0f.js"
+        src="scripts/webflow.js"
         type="text/javascript"
       ></Script>
+      
       <script
         type="text/javascript"
         dangerouslySetInnerHTML={{
