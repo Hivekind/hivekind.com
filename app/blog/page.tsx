64e1f618db8e535c1854e9c869766106c279548c
parasquid <git@parasquid.com>
2024-07-09 16:16:13 +0800

    feat: :sparkles: add blog pages
    content is retrieved from contentful

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,49 +1,175 @@
 import Link from "next/link";
-import { draftMode } from "next/headers";
-
-import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import { marked } from "marked";
 
-// import MoreStories from "../../more-stories";
-// import Avatar from "../../avatar";
-import Date from "@/components/Date";
-// import CoverImage from "../../cover-image";
+import { getAllPosts } from "@/lib/contentfulApi";
+import Image from "next/image";
 
-import { getAllPosts, getPost } from "@/lib/contentfulApi";
-
-export default async function BlogPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const { isEnabled } = draftMode();
-  const { posts } = await getAllPosts();
-
-  console.log(posts);
+export default async function BlogPage() {
+  const { posts } = await getAllPosts({ contentType: "blogPosts" });
 
   return (
-    <div className="container mx-auto px-5">
-      <h2 className="mb-20 mt-8 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
-        <Link href="/" className="hover:underline">
-          Blog
-        </Link>
-        .
-      </h2>
-      {posts.map(({ fields }) => {
-        return (
-          <article key={fields.slug}>
-            <h2>{fields.title}</h2>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: marked.parse(fields.content),
-              }}
-            ></div>
-          </article>
-        );
-      })}
+    <main className="main-wrapper">
+      <header id="blog-header-8" className="section_blog">
+        <div className="padding-global">
+          <div className="container-large">
+            <div className="padding-section-large">
+              <div className="margin-bottom margin-xxlarge">
+                <div className="text-align-center">
+                  <div className="max-width-large align-center">
+                    <div className="margin-bottom margin-small">
+                      <h1>Insights.</h1>
+                    </div>
+                    <p className="text-size-medium">
+                      Discover the Hivekind culture, our processes, findings,
+                      and experiences.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="blog_component">
+                <div className="w-dyn-list">
+                  <div role="list" className="w-dyn-items">
+                    <div role="listitem" className="collection-item w-dyn-item">
+                      <a
+                        href="/blog/test-coverage-with-istanbul-nyc-for-typescript-projects"
+                        className="blog_item-link w-inline-block"
+                      >
+                        <div className="blog_featured-item-wrapper">
+                          <div className="blog_featured-image-wrapper">
+                            <img
+                              alt=""
+                              loading="lazy"
+                              src="https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects.jpg"
+                              sizes="(max-width: 767px) 90vw, 45vw"
+                              srcSet="
+                                  https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects-p-500.jpg   500w,
+                                  https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects-p-800.jpg   800w,
+                                  https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects-p-1080.jpg 1080w,
+                                  https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects-p-1600.jpg 1600w,
+                                  https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects-p-2000.jpg 2000w,
+                                  https://cdn.prod.website-files.com/634908b04a6db34c4c0a6620/666a82d088a0253966a7cb24_test-coverage-with-istanbul-nyc-for-typescript-projects.jpg        2400w
+                                "
+                              className="blog_featured-image"
+                            />
+                          </div>
+                          <div className="blog_featured-item-content background-color-white">
+                            <div className="blog_item-content-top">
+                              <div className="blog_topic-wrapper">
+                                <div className="blog_topic">
+                                  <div className="text-size-small text-weight-semibold">
+                                    Engineering
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="blog_title-wrapper featured-article">
+                                <h2 className="heading-style-h4">
+                                  Test Coverage with Istanbul (NYC) for
+                                  TypeScript Projects
+                                </h2>
+                              </div>
+                              <div className="text-size-regular">
+                                Learn how you can integrate Mocha with NYC to
+                                generate comprehensive test coverage reports for
+                                your TypeScript project.
+                              </div>
+                            </div>
+                            <div className="blog_button-wrapper">
+                              <div className="button is-link is-icon">
+                                <div>Read more</div>
+                                <div className="icon-embed-xxsmall w-embed">
+                                  <svg
+                                    width="16"
+                                    height="16"
+                                    viewBox="0 0 16 16"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path
+                                      d="M6 3L11 8L6 13"
+                                      stroke="CurrentColor"
+                                      stroke-width="1.5"
+                                    />
+                                  </svg>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
 
-      <hr className="border-accent-2 mt-28 mb-24" />
-      {/* <MoreStories morePosts={morePosts} /> */}
-    </div>
+                <div className="w-dyn-list">
+                  <div role="list" className="collection-list-2 w-dyn-items">
+                    {posts.map(({ fields }) => {
+                      return (
+                        <div
+                          role="listitem"
+                          className="w-dyn-item"
+                          key={`${fields.slug}`}
+                        >
+                          <Link
+                            href={`/blog/${fields.slug}`}
+                            className="blog_item-link w-inline-block"
+                          >
+                            <div className="blog_image-wrapper">
+                              <Image
+                                src={fields.mainImage?.fields.file.url}
+                                alt={`${fields.mainImageAltText}`}
+                                width={800}
+                                height={333}
+                                className="blog_image"
+                                loading="lazy"
+                              />
+                            </div>
+                            <div className="blog_item-content background-color-white">
+                              <div className="blog_item-content-top">
+                                <div className="blog_topic-wrapper">
+                                  <div className="blog_topic">
+                                    <div className="text-size-small text-weight-semibold">
+                                      {`${fields.topic}`}
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="blog_title-wrapper">
+                                  <h2 className="heading-style-h5">
+                                    {`${fields.name}`}
+                                  </h2>
+                                </div>
+                              </div>
+                              <div className="blog_button-wrapper">
+                                <div className="button is-link is-icon">
+                                  <div>Read more</div>
+                                  <div className="icon-embed-xxsmall w-embed">
+                                    <svg
+                                      width="16"
+                                      height="16"
+                                      viewBox="0 0 16 16"
+                                      fill="none"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                      <path
+                                        d="M6 3L11 8L6 13"
+                                        stroke="CurrentColor"
+                                        stroke-width="1.5"
+                                      />
+                                    </svg>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </Link>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </main>
   );
 }
--- lib/contentfulApi.ts
+++ lib/contentfulApi.ts
@@ -5,18 +5,24 @@ const contentfulClient = contentful.createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? "",
 });
 
-export async function getAllPosts() {
+export async function getAllPosts({ contentType }: { contentType: string }) {
   const response = await contentfulClient.getEntries({
-    content_type: "blog",
+    content_type: contentType,
   });
 
   return { posts: response.items };
 }
 
-export async function getPost(slug: string, isEnabled: boolean) {
+export async function getPost({
+  contentType,
+  slug,
+}: {
+  contentType: string;
+  slug: string;
+}) {
   const response = await contentfulClient.getEntries({
-    content_type: "blog",
-    "fields.slug[like]": slug,
+    content_type: contentType,
+    "fields.slug[in]": slug,
   });
 
   return { post: response.items[0] };
