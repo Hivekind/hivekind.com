d05a4114c182ab0040b4d19364eeb6a4fedfb6f0
Haritha Kotte <haritha.kotte.k@gmail.com>
2024-07-10 22:19:38 +0530

    Fix lint errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as postmark from "postmark";
-import * as express from "express";
-import * as cors from "cors";
+const express = require("express");
+const cors = require("cors");
+const postmark = require("postmark");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 const app = express();
 app.use(cors({ origin: true }));
@@ -13,20 +13,22 @@ const postmarkClient = new postmark.ServerClient(
   "732a3ccc-ce12-47b6-845a-7469ea8d7b04"
 );
 
-export const sendEmail = functions.https.onCall(async (data) => {
-  const { subject, message } = data;
+export const sendEmail = functions.https.onCall(
+  async (data: { [index: string]: string }) => {
+    const { subject, message } = data;
 
-  try {
-    const result = await postmarkClient.sendEmail({
-      From: "haritha@hivekind.com",
-      To: "haritha@hivekind.com",
-      Subject: subject,
-      HtmlBody: message,
-    });
+    try {
+      const result = await postmarkClient.sendEmail({
+        From: "haritha@hivekind.com",
+        To: "haritha@hivekind.com",
+        Subject: subject,
+        HtmlBody: message,
+      });
 
-    return { success: true, result };
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return { success: false, error };
+      return { success: true, result };
+    } catch (error) {
+      console.error("Error sending email:", error);
+      return { success: false, error };
+    }
   }
-});
+);
