0efcd35e76fc944fd93ef21191fbeecc83b4c6f0
Haritha Kotte <haritha.kotte.k@gmail.com>
2024-07-11 20:16:22 +0530

    replace postmark with mailgun

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,43 @@
 const express = require("express");
 const cors = require("cors");
-const postmark = require("postmark");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
 
 const app = express();
 app.use(cors({ origin: true }));
 
 admin.initializeApp();
+const mailgun = new Mailgun(formData);
 
 exports.sendEmail = functions
   .runWith({
-    secrets: ["POSTMARK_SERVER_TOKEN", "CONTACT_FORM_RECEIVING_EMAIL"],
+    secrets: [
+      "MAILGUN_API_KEY",
+      "MAILGUN_DOMAIN",
+      "CONTACT_FORM_RECEIVING_EMAIL",
+    ],
   })
   .https.onCall(async (data: { [index: string]: string }) => {
     const { subject, message } = data;
-    const postmarkClient = new postmark.ServerClient(
-      process.env.POSTMARK_SERVER_TOKEN
-    );
+    const mg = mailgun.client({
+      username: "api",
+      key: process.env.MAILGUN_API_KEY,
+    });
 
     try {
-      const result = await postmarkClient.sendEmail({
-        From: process.env.CONTACT_FORM_RECEIVING_EMAIL,
-        To: process.env.CONTACT_FORM_RECEIVING_EMAIL,
-        Subject: subject,
-        HtmlBody: message,
-      });
+      const emailData = {
+        from: process.env.CONTACT_FORM_RECEIVING_EMAIL,
+        to: process.env.CONTACT_FORM_RECEIVING_EMAIL,
+        subject: subject,
+        html: message,
+      };
+
+      const result = await mg.messages.create(
+        process.env.MAILGUN_DOMAIN,
+        emailData
+      );
 
       return { success: true, result };
     } catch (error) {
