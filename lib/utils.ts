493ae4e56af056d4cbb6e334cbcb50c855c8e998
parasquid <git@parasquid.com>
2024-07-18 14:08:46 +0800

    feat: :sparkles: retrieve seo meta from contentful via path
    this is mostly used for static pages and aggregates (e.g. /blog or /work)

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+import { ResolvingMetadata } from "next";
+
+export function getPathnameFromState(state: ResolvingMetadata): string {
+  const kResourceStore = Object.getOwnPropertySymbols(state)
+    .map((item) => state[item as keyof ResolvingMetadata])
+    .find((state) => state?.hasOwnProperty?.("urlPathname"));
+
+  const url = new URL(
+    (kResourceStore as any)?.urlPathname ?? "",
+    "http://localhost"
+  );
+
+  return url.pathname;
+}
