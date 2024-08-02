97acb7195f45c10b74a8fdd2e5b4118fa04a3546
Haritha Kotte <haritha.kotte.k@gmail.com>
2024-08-02 14:11:32 +0530

    Setup a fallback image onError

--- components/contentful-image.tsx
+++ components/contentful-image.tsx
@@ -6,13 +6,37 @@ interface ContentfulImageProps {
   src: string;
   width?: number;
   quality?: number;
+  format?: string;
   [key: string]: any; // For other props that might be passed
 }
 
-const contentfulLoader = ({ src, width, quality }: ContentfulImageProps) => {
-  return `${src}?w=${width}&q=${quality || 75}&fm=webp`;
+const contentfulLoader = ({
+  src,
+  width,
+  quality,
+  format,
+}: ContentfulImageProps) => {
+  return `${src}?w=${width}&q=${quality || 75}&fm=${format || "webp"}`;
 };
 
 export default function ContentfulImage(props: ContentfulImageProps) {
-  return <Image alt={props.alt} loader={contentfulLoader} {...props} />;
+  const fallbackImage = contentfulLoader({
+    src: props.src,
+    width: props.width,
+    quality: props.quality,
+    format: "jpg",
+  });
+
+  return (
+    <Image
+      alt={props.alt}
+      loader={contentfulLoader}
+      {...props}
+      onError={(event: React.SyntheticEvent<HTMLImageElement>) => {
+        const target = event.target as HTMLImageElement;
+        target.src = fallbackImage;
+        target.srcset = fallbackImage;
+      }}
+    />
+  );
 }
