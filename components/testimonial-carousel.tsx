a25c7a553ec3b21675a4e637400387ad7568b385
Haritha Kotte <haritha.kotte.k@gmail.com>
2024-10-01 12:38:16 +0530

    remove useState for items

--- components/testimonial-carousel.tsx
+++ components/testimonial-carousel.tsx
@@ -29,7 +29,7 @@ interface CarouselItem {
 }
 
 const TestimonialCarousel: React.FC = () => {
-  const [items, setItems] = useState<CarouselItem[]>([
+  const items = [
     {
       id: 1,
       quote:
@@ -75,7 +75,7 @@ const TestimonialCarousel: React.FC = () => {
       image: mitchGelber,
       imageFallback: mitchGelberFallback,
     },
-  ]);
+  ];
 
   const [activeIndex, setActiveIndex] = useState(0);
 
