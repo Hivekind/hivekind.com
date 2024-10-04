dca383e75dc0fac2a16f4723f9c9eb807bfec8c3
parasquid <tristan.gomez@gmail.com>
2024-10-04 14:00:31 +0800

    feat: :sparkles: add copy for contact us section

--- app/services/ai-product-development/page.tsx
+++ app/services/ai-product-development/page.tsx
@@ -4,16 +4,22 @@ import AIFlowSection from "./ai-flow-section";
 import HeaderSection from "./header-section";
 import StepsSection from "./steps-section";
 import WhySection from "./why-section";
+import ContactUsSection from "@/components/contact-us-section";
 
 export default function AiProductDevelopment() {
   return (
     <>
       <HeaderSection />
       <WhySection />
-      <AIFlowSection />
-      <TestimonialCarousel />
       <StepsSection />
       <BlogSection />
+      <AIFlowSection />
+      <TestimonialCarousel />
+      <ContactUsSection
+        heading="Need help building your product?"
+        body="Reach out to us by filling out the form on our contact page. If you need an NDA, just let us know, and we’ll gladly provide one!"
+        cta="Take me to the contact page"
+      />
     </>
   );
 }
